51b8dfda5d130d3697ddba203ed48e82afb01b8d
Mori Atsushi <ki4070ma@gmail.com>
2019-06-15 09:31:23 +0700

    Initial commit (#1)
    * Initial commit
    
    * review comment
    
    And some changes
    
    * review comments

---
+++ main.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import termios
+import tty
+
+from toio.toio import Toio
+
+UUID_NAME = "TBD"
+
+
+class ReadChar():
+
+    def __enter__(self):
+        self.fd = sys.stdin.fileno()
+        self.old_settings = termios.tcgetattr(self.fd)
+        tty.setraw(sys.stdin.fileno())
+        return sys.stdin.read(1)
+
+    def __exit__(self, type, value, traceback):
+        termios.tcsetattr(self.fd, termios.TCSADRAIN, self.old_settings)
+
+
+def _get_ta_addr():
+    from pygatt.backends.gatttool.gatttool import GATTToolBackend
+    gatttool = GATTToolBackend()
+    devices = gatttool.scan(timeout=2)
+    ret_addr = ""
+    for d in devices:
+        if d['name'] and 'Trolley' in d['name']:
+            print("Toio name: {}, address: {}".format(d['name'], d['address']))
+            ret_addr = d['address']
+    if not ret_addr:
+        print("Toio not found")
+    return str(ret_addr)
+
+
+def main():
+    toio = Toio(_get_ta_addr())
+    if toio.is_connected():
+        toio.request_data(UUID_NAME)
+        toio.straight()
+        toio.disconnect()
+
+
+def main_steps():
+    toio = Toio(_get_ta_addr())
+    if toio.is_connected():
+        toio.straight()
+        toio.turn_right()
+        toio.back()
+        toio.spin_turn_180()
+        toio.disconnect()
+
+
+def main_user_input():
+    toio = Toio(_get_ta_addr())
+    if toio.is_connected():
+        print("w: UP, a: TURN LEFT, d: TURN RIGHT, x: BACK, SHIFT+C: exit")
+        while True:
+            with ReadChar() as rc:
+                char = rc
+            if char == 'w':
+                toio.straight()
+            elif char == 'a':
+                toio.turn_left()
+            elif char == 'd':
+                toio.turn_right()
+            elif char == 'x':
+                toio.back()
+            if char in "^C":
+                break
+        toio.disconnect()
+
+
+def main_test():
+    toio = Toio(_get_ta_addr())
+    if toio.is_connected():
+        toio.back()
+        toio.disconnect()
+
+
+if __name__ == '__main__':
+    # main()
+    # main_steps()
+    main_user_input()
+    # main_test()
---
+++ toio/toio.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from bluetooth.ble import GATTRequester
+
+
+class Toio(object):
+    HANDLER = {'motor': "TBU"}
+    DIRECTION = {'fwd': 1, 'bck': 2}
+    MOTOR = {'1st': 1, '2nd': 2}
+    SPEED_MAX = 100
+
+    def __init__(self, addr):
+        self.req = ""
+        if not addr:
+            return
+        self.req = GATTRequester(addr, False)
+        print("Connecting...: {}".format(addr))
+        self.req.connect(True, 'random')
+        print("Connected: {}".format(addr))
+
+    def request_data(self, uuid):
+        return self.req.read_by_uuid(uuid)[0]
+
+    def write_data(self, handler, data):
+        self.req.write_by_handle(handler, data)
+
+    def disconnect(self):
+        self.req.disconnect()
+
+    def is_connected(self):
+        if not self.req:
+            return False
+        return self.req.is_connected()
+
+    def _move(self, motor_1st_dir, motor_1st_speed, motor_2nd_dir, motor_2nd_speed, duration_sec):
+        self.write_data(self.HANDLER['motor'], str(
+            bytearray([2, self.MOTOR['1st'], motor_1st_dir, motor_1st_speed, self.MOTOR['2nd'], motor_2nd_dir, motor_2nd_speed, int(duration_sec * 100)])))
+        import time
+        time.sleep(duration_sec)
+
+    def straight(self):
+        self._move(self.DIRECTION['fwd'], self.SPEED_MAX, self.DIRECTION['fwd'], self.SPEED_MAX, 1)
+
+    def turn_left(self):
+        self._move(self.DIRECTION['fwd'], self.SPEED_MAX / 2, self.DIRECTION['fwd'], self.SPEED_MAX, 1)
+
+    def turn_right(self):
+        self._move(self.DIRECTION['fwd'], self.SPEED_MAX, self.DIRECTION['fwd'], self.SPEED_MAX / 2, 1)
+
+    def back(self):
+        self._move(self.DIRECTION['bck'], self.SPEED_MAX, self.DIRECTION['bck'], self.SPEED_MAX, 1)
+
+    def spin_turn_180(self):
+        self._move(self.DIRECTION['bck'], self.SPEED_MAX, self.DIRECTION['fwd'], self.SPEED_MAX, 0.5)
+
+    def spin_turn_360(self):
+        self._move(self.DIRECTION['bck'], self.SPEED_MAX, self.DIRECTION['fwd'], self.SPEED_MAX, 1)
